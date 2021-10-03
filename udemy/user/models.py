341b37346edf681521662fbcbe48fa30ec8f25d9
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-03 16:06:35 +0300

    add user model

--- udemy/user/models.py
+++ udemy/user/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models from BaseUserManager
+from udemy.courses.models import Course
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+  use_in_migrations = True
+
+  def create_superuser(self, email, password, name, **other_fields):
+    other_fields.setdefault('is_staff', True)
+    other_fields.setdefault('is_superuser', True )
+
+    if other_fields.get('is_staff') is not True:
+      return ValueError('Superuser must have is_staff True')
+
+    if other_fields.get('is_superuser') is not True:
+      return ValueError('Superuser must have is_speruser True')
+    
+    return self.createuser(email, password, name, **other_fields)
+
+  def create_user(self, email, password, name, **other_fields):
+    if not email:
+      raise ValueError('You must provide a valid email')
+
+    email = self.normalized_email(email)
+
+    user = self.model(email=email, name=name, **other_fields)
+
+    user.set_password(password)
+
+    user.save()
+
+    return user
+
+class User(AbstractBaseUser, PermissionMixin):
+  name = models.CharField(max_length = 255)
+  email = models.EmailField(max_length=255)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  is_staff = models.BooleanField(default=False)
+  paid_courses = models.ManyToManyField(Course)
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['name']
+
+  objects = UserManager()
+
+  def __str__(self) -> str:
+    return self.name + ' ' + self.email
